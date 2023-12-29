68812203a85f7f4c7eca6cbc62a47bcbde8e9a7e
baharkose <baharkse17@gmail.com>
2023-12-29 21:13:36 +0300

    made flex and loading

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import { useMovieContext } from "../context/MovieContext";
+import MovieCard from "../components/MovieCard";
+import { useAuthContext } from "../context/AuthContext";
 
 const Main = () => {
   const {movies, loading} = useMovieContext();
-  return <>
-    {movies.map(movie=><p>{movie.id}</p>)}
-  </>;
+  const {currentUser} = useAuthContext()
+  return <div className="flex flex-wrap justify-center">
+    {
+    loading ? (
+      <div
+      className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-600 mt-52"
+      role="status"
+    >
+      <span className="visually-hidden">Loading...</span>
+    </div>
+    ): (
+     movies.map(movie=>
+      <MovieCard key={movie.id} {...movie}/>
+      ) 
+    )}
+    
+  </div>;
 };
 
 export default Main;
