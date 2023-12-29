bc63231ee876d830ac2a6ab9006b314bcb685e86
baharkose <baharkse17@gmail.com>
2023-12-29 20:21:16 +0300

    moviecontext hazırlandı

---
+++ src/context/MovieContext.jsx
@@ -0,0 +1,12 @@
+import React, { createContext, useContext } from "react";
+
+export const MovieContext = createContext();
+export const useMovieContext = () => {
+  return useContext(MovieContext);
+};
+
+const MovieContextProvider = ({ children }) => {
+  return <MovieContext.Provider value={null}>{children}</MovieContext.Provider>;
+};
+
+export default MovieContextProvider;
