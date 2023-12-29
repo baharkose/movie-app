a663e11685e19e194e945529944946693293d1d3
baharkose <baharkse17@gmail.com>
2023-12-29 20:30:01 +0300

    added movie context

--- src/context/MovieContext.jsx
+++ src/context/MovieContext.jsx
@@ -1,12 +1,28 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useEffect } from "react";
+import axios from "axios";
 
 export const MovieContext = createContext();
 export const useMovieContext = () => {
   return useContext(MovieContext);
 };
 
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+
 const MovieContextProvider = ({ children }) => {
+    useEffect(()=>{
+        getMovies()
+    }, [])
+    // - ilk açılışta gelmesini istediğimiz için useEffect kullandık. 
+  const getMovies = () => {
+    axios
+      .get(FEATURED_API)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
   return <MovieContext.Provider value={null}>{children}</MovieContext.Provider>;
 };
 
 export default MovieContextProvider;
+
+// - burayı da sarmallamayı unutmamalıyız.
